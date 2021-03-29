f0a9459b8ad382a890fb668c4bb9a8460a3078f0
Doug Perez <perezcpt@gmail.com>
2021-03-29 14:57:47 -0700

    testing blah

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-fontawesome-css`
     {
       resolve: `gatsby-source-filesystem`,
       options: {
