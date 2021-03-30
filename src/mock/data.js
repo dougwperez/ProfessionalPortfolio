96bd15249898389f4a81a2fb3ee3349de585c3d2
Doug Perez <perezcpt@gmail.com>
2021-03-29 17:59:56 -0700

    add bigger picture

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile1copy.jpg',
+  img: 'profileXL.jpg',
   paragraphOne:
     'I first started coding Front End applications while quarantining in Wuhan in December 2019, during the first Covid-19 outbreak. During this tense time, I built ESL MiniGames, a suite of engaging, educational games in JavaScript. It was a resounding success that allowed for a more interactive experience over Zoom. From there, my passion for Front End development only grew.',
   paragraphTwo:
