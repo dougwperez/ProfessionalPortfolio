5e337a4c65a050404a3ee86cf355b1fae621ea24
Doug Perez <perezcpt@gmail.com>
2021-06-07 08:21:46 -0700

    edit to text

--- src/mock/data.js
+++ src/mock/data.js
@@ -35,9 +35,9 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project0.jpeg',
-    title: 'MyBuyTech Store',
+    title: 'MyBuyTech eStore',
     info:
-      'A custom made E-commerce site that uses a MERN stack framework. The e-store features high end consumer electronics, PayPal and Visa/Mastercard integration, and user authentication. Users can customize their accounts, make purchases, and leave product reviews. Site architecture relies heavily on Redux.',
+      'A custom made E-commerce site that uses a MERN stack framework. The e-store features high-end consumer electronics, PayPal and Visa/Mastercard integration, and user authentication. Users can customize their accounts, make purchases, and leave product reviews. Site architecture relies heavily on Redux.',
     info2: '',
     url: 'https://mybuytech.herokuapp.com/',
     repo: 'https://github.com/dougwperez/E-Mai-ecommerce', // if no repo, the button will not show up
