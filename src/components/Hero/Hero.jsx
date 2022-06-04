06a0c77d1a9e2a942a81ccc19bf902794749fa10
Doug Perez <56580725+dougwperez@users.noreply.github.com>
2022-06-04 15:59:48 -0700

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -66,7 +66,7 @@ const Header = () => {
       <iframe
         width="100%"
 // NOTE ORIGINAL HEIGHT VALUE WAS 515
-        height="650"
+        height="515"
 loading="lazy"
 //  OLD CUBES
 //         src="https://dougwperez.github.io/Tech-Stack-Cubes/"
